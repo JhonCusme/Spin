111820873745918e3f5a062288d8f16fd8e47044
Jhon <jhonalexandercusmecedeno@gmail.com>
2026-04-28 23:20:52 -0500

    Agregar script para preparar PWA para conversión APK online

---
+++ pwa-for-apk/sw.js
@@ -0,0 +1,12 @@
+﻿self.addEventListener('install', (event) => {
+  console.log('Service Worker installing.');
+});
+
+self.addEventListener('fetch', (event) => {
+  // Para modo offline bÃ¡sico
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return new Response('Offline - ConÃ©ctate a internet para usar SpinDraw');
+    })
+  );
+});
